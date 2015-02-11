02dfdf9293c6c8727a660afe775cbe78eba04bbf
Tony DiPasquale <tony@thoughtbot.com>
2015-02-10 23:50:09 -0500

    Only load google maps api the first time

--- app/routes/locations/new.js
+++ app/routes/locations/new.js
@@ -8,8 +8,12 @@ export default AdministrationRoute.extend({
 
   beforeModel: function () {
     return new Ember.RSVP.Promise( function (resolve, reject) {
-      window.mapAPILoaded = Ember.run.bind(resolve);
-      Ember.$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCYKDmsSlu_GNmW5OHDv_R8VZzhQpHEW9E&sensor=false&callback=mapAPILoaded').fail(reject);
+      if (!window.google) {
+        window.mapAPILoaded = Ember.run.bind(resolve);
+        Ember.$.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCYKDmsSlu_GNmW5OHDv_R8VZzhQpHEW9E&sensor=false&callback=mapAPILoaded').fail(reject);
+      } else {
+        resolve();
+      }
     });
   },
 
