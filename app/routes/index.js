0ae007b856d3526ba585620164a45f4ba3ca5724
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2016-01-24 00:26:55 -0800

    Fix auto transition to events

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,6 @@ import AuthenticationRoute from './authentication';
 
 export default AuthenticationRoute.extend({
   afterModel: function () {
-    this.transitionTo('events').then( (route) => {
-      const controller = route.controllerFor('events');
-      controller.set('date', null);
-      controller.resetCalendar();
-    });
+    this.transitionTo('events');
   }
 });
